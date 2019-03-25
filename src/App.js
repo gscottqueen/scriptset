f0d17610f1f075bfe73f83f529b731f697d1de7c
gscottqueen <geoffreysqueen@gmail.com>
2019-03-24 22:26:16 -0400

    assiging header footer scene states

--- src/App.js
+++ src/App.js
@@ -6,16 +6,26 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = { 
-      script: [DATA],
+      script: DATA,
+      scriptCover: DATA.document.cover,
+      scriptHeader: DATA.document.header,
+      scriptFooter: DATA.document.footer,
+      scriptScenes: DATA.document.scenes,
     }
   }
   
   render() {
     console.log(this.state.script)
+    // console.log(this.state.scriptCover)
+
     return (
       <div className="App">
         <header className="App-header">
-          <div>{this.state.script[0].id}</div>
+          <div>
+            <div>{this.state.scriptCover.title}</div>
+            <div>{this.state.scriptCover.additional}</div>
+            <div>{this.state.scriptCover.author}</div>
+          </div>
         </header>
       </div>
     );
