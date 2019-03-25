137e66ac871e6987930542e95f1a0b80a5498360
gscottqueen <geoffreysqueen@gmail.com>
2019-03-24 22:50:59 -0400

    printing more header content

--- src/App.js
+++ src/App.js
@@ -16,16 +16,30 @@ class App extends Component {
   
   render() {
     console.log(this.state.script)
-    console.log(this.state.scriptCover)
+    // console.log(this.state.scriptCover)
 
     return (
       <div className="App">
         <header className="App-header">
           <div>
             <div>{this.state.scriptCover.title}</div>
-            <div>{this.state.scriptCover.additional}</div>
-            <div>{this.state.scriptCover.author}</div>
+            <div>{this.state.scriptCover.additional ? this.state.scriptCover.additional : null}</div>
+            <div>{this.state.scriptCover.author ? this.state.scriptCover.author : 'unknown'}</div>
           </div>
+            {this.state.scriptScenes.map(function(scene) {
+              console.log(scene.heading)
+              return (
+                <div>
+                  <div>{scene.heading.title}</div>
+                  <div>{scene.heading.description[0]}</div>
+                  <div>{scene.heading.description[1]}</div>
+                  <div>{scene.heading.description[2]}</div>
+                  <div>{scene.heading.setting.title}</div>
+                  <div>{scene.heading.sequence.en ? scene.heading.sequence.en : null}</div>
+                  {/* <div>{scene.heading.setting.en ? scene.heading.settings.en : null}</div> */}
+                </div>
+              )
+            })}
         </header>
       </div>
     );
