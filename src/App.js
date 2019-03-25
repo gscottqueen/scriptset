259838b68d5f017ad6f095f7b71eb37ee6580a53
gscottqueen <geoffreysqueen@gmail.com>
2019-03-24 22:10:13 -0400

    getting our json printing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import DATA from './farmland.json'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+      script: [DATA],
+    }
+  }
+  
   render() {
+    console.log(this.state.script)
     return (
       <div className="App">
         <header className="App-header">
-          Scriptset
+          <div>{this.state.script[0].id}</div>
         </header>
       </div>
     );
