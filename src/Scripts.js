df9d8296551bf193b60c514aa3b416990bfb0cf1
gscottqueen <geoffreysqueen@gmail.com>
2019-10-14 20:06:40 -0400

    added new scripts listing compoenent

---
+++ src/Scripts.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Script extends Component {
+  render() {
+    return (
+      <div>
+        <ul className="scripts-list">
+          <li>
+            <button>Farmland</button>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Script;
