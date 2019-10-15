9a76351590ebd2579c84719080cba146ca5b54e4
gscottqueen <geoffreysqueen@gmail.com>
2019-10-14 22:15:01 -0400

    setting state with scripts array

--- src/Scripts.js
+++ src/Scripts.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 
+// Bring in our scripts
+// const scriptsContext = require.context('./Data/Scripts', true, /\.json$/);
+// const scriptArray = scriptsContext.keys().map((scripts) => scriptsContext(scripts));
+const importAll = (r) => r.keys().map(r);
+const scriptArray = importAll(require.context('./Data/Scripts', true, /\.json$/));
 
-class Script extends Component {
+class Scripts extends Component {
+
+  state = {
+    scripts: scriptArray,
+  }
+
   render() {
+    //map our scripts
+    console.log(this.state.scripts);
     return (
       <div>
         <ul className="scripts-list">
-          <li>
-            <button>Farmland</button>
-          </li>
+          {this.state.scripts.map((script, index) =>
+            <li index={index}>
+              <button>{script.title}</button>
+            </li>
+          )}
         </ul>
       </div>
     );
   }
 }
 
-export default Script;
+export default Scripts;
